cad32a8bda8109cf63139e699d751885f7a91879
husen ikary <husenikary@mail.ru>
2025-06-12 23:54:39 +0300

    change

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
   return (
-    <section className="wrapper">
-      <h1 className='home_h1'>Hero</h1>
-    </section>
+    <main className="global_backfround_gradient">
+      <section className='wrapper home_section'>
+        Hero
+      </section>
+    </main>
   );
 };
 
